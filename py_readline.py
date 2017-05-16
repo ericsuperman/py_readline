785826f073f47d231a0c55b266d7f96df46f8778
Eric Chang <Eric Chang>
2017-05-16 16:45:06 +0800

    add reading file by lines

--- py_readline.py
+++ py_readline.py
@@ -4,5 +4,13 @@ import sys
 
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
+if len(sys.argv) != 2:
+	print ("Usage:\n") 
+	sys.exit()
 
+with open(sys.argv[1]) as f:
+    for line in f:
+        print(line) 
+
+f.close()
 
